9e7ab9b0d2bcfc88c273f27572275647d340b680
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-08-25 09:29:57 +0900

    upload setup and LOCENSE

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+setuptools.setup(
+    name="dg-drawer",
+    version="1.0.0",
+    description="Drawing module for NII Data Governance",
+    license='Apache License 2.0',
+    author="NII",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NII-DG/dg-drawer.git",
+    python_requires=">=3.8",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)

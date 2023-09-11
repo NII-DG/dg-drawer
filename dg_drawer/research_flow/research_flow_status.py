66420e027354b20f282234352ffe7a910652f546
Dai Tsukioka <dai.tsukioka@ivis.co.jp>
2023-09-11 15:09:22 +0900

    update dg-drawer

---
+++ dg_drawer/research_flow/research_flow_status.py
@@ -0,0 +1,54 @@
+
+from typing import List
+import json
+
+class SubFlowStatus():
+    def __init__(self, id:str, name:str, link:str, parent_ids:List[str], create_datetime:int) -> None:
+        self._id = id
+        self._name = name
+        self._link = link
+        self._parent_ids = parent_ids
+        self._create_datetime = create_datetime
+
+
+class PhaseStatus():
+
+    def __init__(self, seq_number:int, name:str, sub_flow_data: List[SubFlowStatus]) -> None:
+        self._seq_number = seq_number
+        self._name = name
+        self._sub_flow_data = sub_flow_data
+
+    def update_name(self, name:str):
+         self._name = name
+
+class ResearchFlowStatus():
+
+
+    @classmethod
+    def load_from_json(cls, json_path:str)->List[PhaseStatus]:
+
+        with open(json_path, 'r') as f:
+            loaded_json = json.load(f)
+
+        ## TODO : check property : key and type
+
+        ## get phase status list from ['research_flow_pahse_data']
+        phase_list = loaded_json['research_flow_pahse_data']
+
+        research_flow_status = []
+        for phase in phase_list:
+            sub_flow_data = []
+            for sub_flow in phase['sub_flow_data']:
+                sfs = SubFlowStatus(
+                        sub_flow['id'],
+                        sub_flow['name'],
+                        sub_flow['link'],
+                        sub_flow['parent_ids'],
+                        sub_flow['create_datetime']
+                    )
+                sub_flow_data.append(sfs)
+            ps = PhaseStatus(phase['seq_number'], phase['name'], sub_flow_data)
+            research_flow_status.append(ps)
+
+        sorted_research_flow_status = sorted(research_flow_status, key=lambda x : x._seq_number)
+        return sorted_research_flow_status
